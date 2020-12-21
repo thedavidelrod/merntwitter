2612b758d865a3ad459f1dbc2e60be2174facbdb
“delrod” <thedavidelrod@gmail.com>
2020-12-21 14:24:23 -0900

    git init

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("It's a glizzy world"));
+
+const db = require("./config/keys").mongoURI;
+app.get("/", (req, res) => res.send("It's a glizzy world baby, we just live here!"));
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
